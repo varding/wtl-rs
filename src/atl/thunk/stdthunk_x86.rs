d5c5a5eec58c9ffefcc8456dc5b6b80b099ae8ce
ding <deroculs@gmail.com>
2015-09-29 20:14:43 +0800

    test thunk - x86

--- src/atl/thunk/stdthunk_x86.rs
+++ src/atl/thunk/stdthunk_x86.rs
@@ -1,10 +1,11 @@
 
-
-
+#![allow(non_snake_case,unused_variables,dead_code)]
 #[cfg(target_arch = "x86")]
 pub mod imp {
     use std;
-    use winapi::{USHORT, ULONG64};
+    use winapi::{USHORT, ULONG64, c_void, DWORD_PTR, LPCVOID, SIZE_T};
+    use kernel32;
+    use std::fmt;
 	//use kernel32;
 
     #[repr(C,packed)]
@@ -19,6 +20,24 @@ pub mod imp {
 	impl Thunk {
         pub fn print(&self) {
             println!("print x86:{}",std::mem::size_of::<Thunk>());
+        }
+
+        pub fn init(&mut self, func: DWORD_PTR, p_this: *const c_void) {
+            self.m_mov = 0x042444C7;
+            self.m_this = p_this as ULONG_PTR as DWORD;
+            self.m_jmp = 0xe9;
+            self.m_relproc = (func as INT_PTR - (self as *const _ as INT_PTR + std::mem::size_of::<Thunk>() as INT_PTR)) as DWORD;
+            unsafe {
+                let p = self as *const Thunk;
+                kernel32::FlushInstructionCache(kernel32::GetCurrentProcess(),
+                                                p as LPCVOID,
+                                                std::mem::size_of::<Thunk>() as SIZE_T);
+            }
+            //println!("{}", std::mem::size_of_val(self));
+        }
+
+        pub fn GetCodeAddress(&self) -> *const Thunk {
+            self as *const Self
         }
 	}
 }
