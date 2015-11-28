ca14d1f704df40d9cb921f53d96221a608eca397
ding <deroculs@gmail.com>
2015-11-28 15:53:02 +0800

    add windres target

--- tools/ui_gen/build.rs
+++ tools/ui_gen/build.rs
@@ -12,13 +12,43 @@ fn link_res(rc_path:&str){
     let rc_name = p.file_name().expect("path error").to_str().expect("parse error");
     let out_dir = env::var("OUT_DIR").ok().expect("can't find out_dir");
 
+    //check if rustc is x86
+    //let c = 
+    // let rustc_out = Command::new("rustc").arg("-vV").output().unwrap().stdout;
+    // let rustc_str = String::from_utf8_lossy(&rustc_out);
+    // let lines: Vec<&str> = rustc_str.lines().collect();
+
+    // let is_x64_rustc = false;
+    // for l in lines {
+    //   if l.starts_with("host") {
+    //     if let Some(p) = l.find("x86_64") {
+    //       is_x64_rustc = true;
+    //       panic!("x64");
+    //     }
+    //   }
+    // }
+    
+    //check if built by appveyor
+    // if let Ok(a) = env::var("appveyor") {
+    //   panic!("in appveyor");
+    // }else{
+
+    // }
+    //panic!("{:?}", env::var("mytarget"));
     let obj_path = &format!("{}/{}.rc.o", out_dir,rc_name);
 
     let lib_path = &format!("{}/lib{}.a",out_dir,rc_name);
 
-    Command::new("windres").args(&[&*rc_path,  "-o"])
-                          .arg(obj_path)
-                          .status().unwrap();
+    // if is_x64_rustc {
+
+    // }
+    Command::new("windres").arg("-F").arg("pe-x86-64")
+                .arg(obj_path)
+                .status().unwrap();
+
+    // Command::new("windres").args(&[&*rc_path,  "-o"])
+    //                       .arg(obj_path)
+    //                       .status().unwrap();
 
     Command::new("ar").args(&["crus", lib_path, obj_path])
                       .current_dir(&Path::new(&out_dir))
